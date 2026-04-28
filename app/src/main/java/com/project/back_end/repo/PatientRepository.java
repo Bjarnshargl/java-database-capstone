530557b7f523e59fab9b0d3adcd8cdeeeff4684e
Bjarnshargl <bjarnshargl@gmail.com>
2026-04-28 19:18:51 +0200

    added some comments

--- app/src/main/java/com/project/back_end/repo/PatientRepository.java
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -4,13 +4,28 @@ import com.project.back_end.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for accessing Patient entities.
+ * Provides methods to retrieve Patients by various identifiers.
+ */
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
-    // Retrieves a Patient by their email address
+    /**
+     * Retrieves a Patient by their email address.
+     * @param email the email address of the Patient
+     * @return the Patient object if found, or null if not found
+     */
     Patient findByEmail(String email);
 
-    // Retrieves a Patient by either their email address or phone number
+    /**
+     * Retrieves a Patient by either their email address or phone number.
+     * This method can return a Patient based on either identifier.
+     * Note: If both identifiers match, the first found Patient will be returned.
+     * @param email the email address of the Patient
+     * @param phone the phone number of the Patient
+     * @return the Patient object if found, or null if not found
+     */
     Patient findByEmailOrPhone(String email, String phone);
 
 }
